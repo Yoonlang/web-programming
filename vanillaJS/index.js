2d64e344c3e30dc995a4927aee6e547ddd98e253
Yoonlang <59612396+Yoonlang@users.noreply.github.com>
2020-05-19 23:51:38 +0900

    Learn about variables and function, array, object
    Remember variable name is started by lower case and when you need space-bar, then give upper case to next words.
    Please don't forget `` it is very very powerful when you use String.
    when you make function included by object, you use  var: function(){}, don't forget (,) comma when you use array and objects.

--- vanillaJS/index.js
+++ vanillaJS/index.js
@@ -1,4 +1,42 @@
-console.log('Im Working. Im JS. Im Beautiful');
-let a = 211;
+const a = 211;
 let b = a - 5;
 console.log(a, b);
+
+// let이 꼭 필요한게 아니라면 const 쓰기
+
+//변수명은 lower case로 쓰고 띄어쓰기할때 대문자 쓰기
+const daysOfWeek = ["Mon", "Tus", "Wen", "Thr", "Fri", "Sat", "Sun", true, 13, a, b];
+console.log(daysOfWeek);
+
+//object는 {}
+const yoonInfo = {
+  name:"Yoon",
+  age:22,
+  gender:"male"
+}
+console.log(yoonInfo.age);
+yoonInfo.age = 25;
+console.log(yoonInfo.age);
+// yoonInfo라는 object가 const라도 내부의 변수는 변경 가능하다!!
+//object 내부에 배열도 가능, object를 배열로 나열 가능.
+//array와 object 내부에 콤마(,) 까먹지 말기
+
+function sayHello(name, age){
+  return `Hello ${name} you are ${age} years old`;
+}
+
+console.log(sayHello("Yoon", 22));
+
+
+const calculator = {
+  plus: function(a, b){
+    return a + b;
+  },
+  minus: function(a, b){
+    return a - b > 0 ? a - b : b - a;
+  }
+}
+const plus = calculator.plus(5, 10);
+const minus = calculator.minus(1, 10);
+console.log(plus);
+console.log(minus);
