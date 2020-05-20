63c278b061851fcdcb232003002fbd6c20d90487
Yoonlang <59612396+Yoonlang@users.noreply.github.com>
2020-05-21 00:01:57 +0900

    Separate CSS and Javascript
    CSS has his own works and Javascript also.
    So I separated each other, and I will learn about how to fixed class effects.

--- vanillaJS/index.js
+++ vanillaJS/index.js
@@ -1,20 +1,20 @@
 const title = document.querySelector("#title");
-title.innerHTML = "Hi from JS";
-console.log(title);
-title.style.color = "#ffd800";
 
-document.title = 'Javascript';
-//Javascript를 배우는 이유 : 어떻게 HTML을 수정할 수 있는지도 맞지만
-// 이것보단 이벤트에 반응하기 위해서.
+const CLICKED_CLASS = "clicked";
 
-function handleResize(){
-  console.log("I have been resized");
-
+function handleClick(){
+  const currentClass = title.className;
+  if(currentClass !== CLICKED_CLASS){
+    title.className = CLICKED_CLASS;
+  }
+  else{
+    title.className = "";
+  }
 }
+// 이전 클래스를 존중해주지 않아서 css 의 class를 설정했을 때,
+// class가 바뀌면 이전 것이 사라진다. 해결책은? 2.7 7:03
 
-
-//이거는 handleResize함수를 필요할 때 호출하는 것
-window.addEventListener("resize", handleResize);
-
-//이거는 handleResize함수를 바로 호출하는것
-//window.addEventListener("resize", handleResize());
+function init(){
+  title.addEventListener("click", handleClick);
+}
+init();
