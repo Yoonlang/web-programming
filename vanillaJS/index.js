04fdda43ac1394fdc5d75309d0cd2cc2cb53c049
Yoonlang <59612396+Yoonlang@users.noreply.github.com>
2020-05-22 22:58:10 +0900

    Update index.js

--- vanillaJS/index.js
+++ vanillaJS/index.js
@@ -11,8 +11,7 @@ function handleClick(){
     title.className = "";
   }
 }
-// 이전 클래스를 존중해주지 않아서 css 의 class를 설정했을 때,
-// class가 바뀌면 이전 것이 사라진다. 해결책은? 2.7 7:03
+3
 
 function init(){
   title.addEventListener("click", handleClick);
