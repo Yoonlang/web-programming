06f3af3f44e332d097a12406a69f1a36dfb53b9b
Yoonlang <59612396+Yoonlang@users.noreply.github.com>
2020-05-27 22:10:11 +0900

    Learn vanillaJavascript classList
    I learn about classList method, and first, I use classList.add, .remove, .contain, and learn about .toggle last.
    It's too easy to understand of nomad coder's teaching.

--- vanillaJS/index.js
+++ vanillaJS/index.js
@@ -2,16 +2,20 @@ const title = document.querySelector("#title");
 
 const CLICKED_CLASS = "clicked";
 
+/*
 function handleClick(){
-  const currentClass = title.className;
-  if(currentClass !== CLICKED_CLASS){
-    title.className = CLICKED_CLASS;
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  if(!hasClass){
+    title.classList.add(CLICKED_CLASS);
   }
   else{
-    title.className = "";
+    title.classList.remove(CLICKED_CLASS);
   }
 }
-3
+*/
+function handleClick(){
+  title.classList.toggle(CLICKED_CLASS);
+}
 
 function init(){
   title.addEventListener("click", handleClick);
