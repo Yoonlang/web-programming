20d418131600705f1a349da11a94abe4d770e02c
Yoonlang <59612396+Yoonlang@users.noreply.github.com>
2020-06-28 23:14:29 +0900

    Update todo.js

--- vanillaJS/todo.js
+++ vanillaJS/todo.js
@@ -1,7 +1,7 @@
 const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
-
+   
 const TODOS_LS = 'toDos';
  
 const toDos = [];
